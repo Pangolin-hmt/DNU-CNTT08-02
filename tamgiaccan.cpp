87edd3df674106ee1ba7c0d5965a60cece9dae2e
pangolin-hmt <laodai1691@gmail.com>
2015-03-25 22:40:53 +0700

    update

--- tamgiaccan.cpp
+++ tamgiaccan.cpp
@@ -7,8 +7,7 @@ void main()
 	for (i = d; i >= 1; i--)
 	{
 		for (j =i; j >= 1; j--)cout << "  ";
-		for (j = d - i; j >= 0; j--)cout << "* ";
-		for (j = d - i; j >= 1; j--)cout << "* ";
+		for (j = 2 * (d - i); j >= 0; j--)cout << "* ";
 		cout << "\n";
 	}
 	cin.get();
